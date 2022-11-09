a70cdc85fb92d8892cded63879074409879bf040
rojaDhana <117796628+rojaDhana@users.noreply.github.com>
2022-11-09 07:24:19 +0000

    Tester2

--- src/test/java/testScripts/GooglePageTest.java
+++ src/test/java/testScripts/GooglePageTest.java
@@ -4,6 +4,7 @@ public class GooglePageTest {
 
 	public static void main(String[] args) {
 		System.out.println("Git Integration Update");
+		System.out.println("Git Integration modify tester");
 System.out.println("Rosedha");
 	}
 
